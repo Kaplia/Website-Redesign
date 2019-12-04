a8b8fade3ec09699fa9d08e7d188470499cc7a82
Nadia Kaplia <kapelkanadia@gmail.com>
2019-12-05 00:18:36 +0200

    3 lesson

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="container">
+    <div v-show="true" class="container">
         <input id="menu__toggle" type="checkbox"/>
         <label class="menu__btn" for="menu__toggle">
             <span></span>
         </label>
-        <div class="box-1 menu_box">
+        <div class="sidebar menu_box">
             <div id="header-1">
                 <img src="./../assets/menu-logo.png" alt="">
                 <p>PROJECTUS</p>
@@ -37,7 +37,7 @@
             </div>
         </div>
 
-        <div class="box-2">
+        <div class="navbar">
             <div id="header-2">
                 <div class="logo">
                     <div class="contacts">
@@ -55,9 +55,9 @@
 
                 <div class="navigation">
                     <ul>
-                        <li><a href="">Tasks</a></li>
+                        <li><a href="#" v-on:click="openbox('card-tasks')">Tasks</a></li>
                         <li><a href="">Kanban</a></li>
-                        <li><a href="">Activity</a></li>
+                        <li><a href="#" v-on:click="openbox('card-activity')">Activity</a></li>
                         <li><a href="">Calendar</a></li>
                         <li><a href="">Files</a></li>
                     </ul>
@@ -65,34 +65,50 @@
             </div>
             <div id="container">
                 <div class="content">
-                    <div class="day">TODAY</div>
-                    <div class="activity-tasks" v-for="qwe in asd" :key="qwe.name">
-                        <div class="box box-1">
-                            <h1> {{qwe.task}} </h1>
-                        <!--<h2></h2>-->
-                        <!--<h3></h3>-->
+                    <div id="card-activity" style="display: none;" class="card">
+                        <div class="day">TODAY</div>
+                        <div class="activity-tasks" v-for="taskActivity in activity" :key="taskActivity.name">
+                            <div class="box box-1">
+                                <h1> {{taskActivity.task}} </h1>
+                                <!--<h2></h2>-->
+                                <!--<h3></h3>-->
+                            </div>
+                            <div class="box box-2">
+                                <p> {{taskActivity.time}} </p>
+                            </div>
                         </div>
-                        <div class="box box-2">
-                            <p> {{qwe.time}} </p>
+
+                        <div class="task">
+                            <p>During a project build, it is necessary to
+                                evaluate the product design and development
+                                against project requirements and outcomes</p>
+                        </div>
+
+                        <p class="image">
+                            <img src="./../assets/image-1.jpg" alt="Фотография 1" width="100" height="100"
+                                 v-on:click="imageClick(0)">
+                            <img src="./../assets/image-2.jpg" alt="Фотография 2" width="100" height="100"
+                                 v-on:click="imageClick(1)">
+                            <img src="./../assets/image-3.jpg" alt="Фотография 3" width="100" height="100"
+                                 v-on:click="imageClick(2)">
+                            <img src="./../assets/image-4.jpg" alt="Фотография 4" width="100" height="100"
+                                 v-on:click="imageClick(3)">
+                        </p>
+                    </div>
+                    <div id="card-tasks" style="display: none;" class="card">
+                        <div class="activity-tasks" v-for="task in taskList" :key="task.name">
+                            <div class="box task-name">
+                                {{task.name}}
+                            </div>
+                            <div class="box task-description">
+                                {{task.description}}
+                            </div>
+                            <div class="box task-deadline">
+                                {{task.deadline}}
+                            </div>
                         </div>
                     </div>
 
-                    <div class="task">
-                        <p>During a project build, it is necessary to
-                            evaluate the product design and development
-                            against project requirements and outcomes</p>
-                    </div>
-
-                    <p class="image">
-                        <img src="./../assets/image-1.jpg" alt="Фотография 1" width="100" height="100"
-                             v-on:click="imageClick(0)">
-                        <img src="./../assets/image-2.jpg" alt="Фотография 2" width="100" height="100"
-                             v-on:click="imageClick(1)">
-                        <img src="./../assets/image-3.jpg" alt="Фотография 3" width="100" height="100"
-                             v-on:click="imageClick(2)">
-                        <img src="./../assets/image-4.jpg" alt="Фотография 4" width="100" height="100"
-                             v-on:click="imageClick(3)">
-                    </p>
                 </div>
             </div>
         </div>
@@ -100,14 +116,6 @@
 </template>
 
 <script>
-    // import project from './project.js'
-
-    // export default {
-    //   name: 'Project',
-    //   props: {
-    //     msg: String
-    //   },
-    // }
     let completedTasks = document.querySelector('.completed-tasks');
     let openTasks = document.querySelector('.open-tasks');
 
@@ -127,7 +135,7 @@
                       completed: "237"
                     }
                 ],
-                asd: [
+                activity: [
                     {
                         task: "Darika Samak mark as done Listing on Product Hunt so that we can reach as many potential users",
                         time: "8:40 PM"
@@ -139,6 +147,23 @@
                     {
                         task: "Darika Samak uploaded 4 files on An option to search in current projects or in all projects",
                         time: "6:02 PM"
+                    },
+                ],
+                taskList: [
+                    {
+                        name: "1 task",
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                        deadline: "1.12.2019"
+                    },
+                    {
+                        name: "2 task",
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                        deadline: "20.12.2019"
+                    },
+                    {
+                        name: "3 task",
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                        deadline: "1.01.2020"
                     },
                 ]
             };
@@ -160,8 +185,19 @@
             },
             imageClick(id) {
                 document.querySelector('.notifications').innerHTML = id;
+            },
+            openbox(id) {
+                var all = document.querySelectorAll(".card");
+                for (var i = 0; i < all.length; i++) {
+                     if (all[i].id == id) {
+                         all[i].style.display = 'block'
+                     } else {
+                         all[i].style.display = 'none';
+                     }
+                }
             }
         }
+
     }
 </script>
 
